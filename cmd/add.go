b9289e5ff15c22319f40d9ae1db2ec8b9c48eb50
Harshal Ranjhani <ranjhaniharshal@gmail.com>
2025-10-06 23:18:54 +0530

    feat: add --passphrase-stdin and --passphrase-file support in the init command
    * feat: add more templates
    
    * refactor: update the help section for the scaffold command with new templates
    
    * feat: add flags for recursive directory and hidden file / directory support
    
    * feat: add symlink and directory support for the add command
    
    * fix: remove insecure --passphrase-value flag
    
    * feat: add --passphrase-stdin and --passphrase-file support in the init command
    
    * test: add tests for ui/passphrase.go

--- cmd/add.go
+++ cmd/add.go
@@ -499,9 +499,10 @@ func init() {
 	// Optional flags for the add command
 	addCmd.Flags().BoolP("force", "f", false, "Force add without confirmation")
 	addCmd.Flags().StringP("tags", "t", "", "Comma-separated tags to associate with the file")
-	addCmd.Flags().StringP("passphrase-value", "p", "", "Passphrase for encrypted vault (if required)")
 	addCmd.Flags().BoolP("recursive", "r", false, "Recursively add directories")
 	addCmd.Flags().BoolP("include-hidden", "H", false, "Include hidden files and directories")
+	addCmd.Flags().Bool("passphrase-stdin", false, "Read passphrase from stdin (for automation)")
+	addCmd.Flags().String("passphrase-file", "", "Read passphrase from file (file should have 0600 permissions)")
 }
 
 //TODO: Need to check how symlinks will be handled
